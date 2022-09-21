ad4cf91a194e9fb93dca7ded0c7ae03d9cb1dab1
Mackenzie Leng <kenz.joy@outlook.com>
2022-09-20 22:45:36 -0600

    completed iteration 3 and 4

--- lib/market.rb
+++ lib/market.rb
@@ -20,13 +20,69 @@ class Market
 
   def vendors_that_sell(item)
     vendors.find_all do |vendor|
-      vendor.inventory.include?(item)
+      vendor.check_stock(item) > 0
+      # vendor.inventory.include?(item)
     end
   end
 
-  # def total_inventory
-  #   market_inventory = Hash.new(0)
-  #   market_inventory[item] = Hash.new(0)
-  #   market_inventory[item] = :quantity => 
-  # end
+  def total_inventory
+    item_list = inventory_list 
+    inventory_hash = Hash.new(0)
+    item_list.each do |item|
+      inventory_hash[item] = { quantity: total_item(item), 
+                               vendors: vendors_that_sell(item) }
+    end
+    inventory_hash
+  end
+
+  def total_item(item)
+    quantity_avail = 0
+    vendors.each do |vendor|
+      quantity_avail += vendor.check_stock(item)
+    end
+    quantity_avail
+  end
+
+  def inventory_list
+    list = []
+    vendors.each do |vendor|
+      list << vendor.inventory.keys
+    end
+    list.flatten.uniq
+  end
+
+  def overstocked_items
+    total_inventory.select do |item, details|
+      details[:quantity] > 50 && details[:vendors].count >= 2
+    end.keys
+  end
+
+  def sorted_item_list
+    total_inventory.keys.map do |item|
+      item.name
+    end.sort
+  end
+
+  def date 
+    # date = Date.today.to_s
+    # "#{date[8..9]}/#{date[5..6]}/#{date[0..3]}"
+    Date.today.strftime('%d/%m/%Y')
+  end
+
+  def sell(item, quantity)
+    return false if sorted_item_list.none?(item.name)
+    return false if total_inventory[item][:quantity] < quantity
+
+    total_inventory[item][:vendors].each do |vendor|
+      if vendor.inventory[item] < quantity
+        quantity -= vendor.inventory[item]
+        vendor.inventory[item] = 0
+      else
+        vendor.inventory[item] -= quantity
+        break
+      end
+    end
+    true
+  end
+
 end
--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -1,3 +1,4 @@
+require 'date'
 require './lib/item'
 require './lib/vendor'
 require './lib/market'
@@ -12,6 +13,7 @@ RSpec.describe Market do
     @item2 = Item.new({name: 'Tomato', price: "$0.50"})
     @item3 = Item.new({name: "Peach-Raspberry Nice Cream", price: "$5.30"})
     @item4 = Item.new({name: "Banana Nice Cream", price: "$4.25"})
+    @item5 = Item.new({name: 'Onion', price: '$0.25'})
   end
 
     it 'exists' do 
@@ -71,32 +73,109 @@ RSpec.describe Market do
     end
   end
 
-  # describe '#total_inventory' do 
-  #   it 'returns a hash of market items as keys and a sub-hash as
-  #       values. sub-hash should have two key-value pairs
-  #       quantity pointing to total inventory for that item and 
-  #       vendors pointing to an array of vendors that sell that item' do
-  #         @market.add_vendor(@vendor1)
-  #         @vendor1.stock(@item1, 35)
-  #         @vendor1.stock(@item2, 7)
-  #         @market.add_vendor(@vendor2)
-  #         @vendor2.stock(@item4, 50)
-  #         @vendor2.stock(@item3, 25)
-  #         @market.add_vendor(@vendor3)
-  #         @vendor3.stock(@item1, 65)
-  #         @vendor3.stock(@item3, 10)
+  describe '#total_inventory' do 
+    it 'returns a hash of market items as keys and a sub-hash as
+        values. sub-hash should have two key-value pairs
+        quantity pointing to total inventory for that item and 
+        vendors pointing to an array of vendors that sell that item' do
+          @market.add_vendor(@vendor1)
+          @vendor1.stock(@item1, 35)
+          @vendor1.stock(@item2, 7)
+          @market.add_vendor(@vendor2)
+          @vendor2.stock(@item4, 50)
+          @vendor2.stock(@item3, 25)
+          @market.add_vendor(@vendor3)
+          @vendor3.stock(@item1, 65)
+          @vendor3.stock(@item3, 10)
 
-  #         expected = { 
-  #           @item1 = { :quantity => 100, :vendors => [@vendor1, @vendor3] },
-  #           @item2 = { :quantity => 7, :vendors => [@vendor1] },
-  #           @item3 = { :quantity => 35, :vendors => [@vendor2, @vendor3] },
-  #           @item4 = { :quantity => 50, :vendors => [@vendor2]}
-  #         }
+          expected_hash = { 
+            @item1 => { 
+              quantity: 100, 
+              vendors: [@vendor1, @vendor3] 
+              },
+            @item2 => { 
+              quantity: 7, 
+              vendors: [@vendor1] 
+              },
+            @item4 => { 
+              quantity: 50, 
+              vendors: [@vendor2] 
+              },
+            @item3 => { 
+              quantity: 35, 
+              vendors: [@vendor2, @vendor3] 
+              }
+          }
 
-  #         expect(@market.total_inventory)to eq expected
-     
-  #       end
-  # end
+          expect(@market.total_inventory).to eq(expected_hash)
+    end
+  end
 
-  
+  describe '#overstocked_items' do 
+    it 'returns overstocked items. an item is overstocked
+        if it is sold by more than 1 vendor and the total
+        quantity is greater than 50.' do
+          @market.add_vendor(@vendor1)
+          @vendor1.stock(@item1, 35)
+          @vendor1.stock(@item2, 7)
+          @market.add_vendor(@vendor2)
+          @vendor2.stock(@item4, 50)
+          @vendor2.stock(@item3, 25)
+          @market.add_vendor(@vendor3)
+          @vendor3.stock(@item1, 65)
+          @vendor3.stock(@item3, 10)
+
+        expect(@market.overstocked_items).to eq([@item1])
+    end
+  end
+
+  describe '#sorted_item_list' do 
+    it 'returns a list of names of all items the vendors have
+        in stock, sorted alphabetically, no duplicates.' do 
+          @market.add_vendor(@vendor1)
+          @vendor1.stock(@item1, 35)
+          @vendor1.stock(@item2, 7)
+          @market.add_vendor(@vendor2)
+          @vendor2.stock(@item4, 50)
+          @vendor2.stock(@item3, 25)
+          @market.add_vendor(@vendor3)
+          @vendor3.stock(@item1, 65)
+          @vendor3.stock(@item3, 10)
+
+          expect(@market.sorted_item_list).to eq(["Banana Nice Cream", "Peach", "Peach-Raspberry Nice Cream", "Tomato"])
+        end
+  end
+
+  describe '#date' do
+    it 'return a date for the market' do 
+      allow(Date).to receive(:today).and_return(Date.new(2022, 9, 20))
+      expect(@market.date).to eq('20/09/2022')
+    end
+  end
+
+  describe '#sell' do
+    it 'returns false if the market does not have enough of the item 
+        in stock to satisfy the given quantity, or returns true if the
+        market has enough stock to satisfy the given quantity. Reduces the
+        stock of the vendors in the order that the vendors were added.' do
+          @vendor1.stock(@item1, 35)
+          @vendor1.stock(@item2, 7)
+          @vendor2.stock(@item4, 50)
+          @vendor2.stock(@item3, 25)
+          @vendor3.stock(@item1, 65)
+          
+          @market.add_vendor(@vendor1)
+          @market.add_vendor(@vendor2)
+          @market.add_vendor(@vendor3)
+          
+          expect(@market.sell(@item1, 200)).to eq(false)
+          expect(@market.sell(@item5, 1)).to eq(false)
+          expect(@market.sell(@item4, 5)).to eq(true)
+          expect(@vendor2.check_stock(@item4)).to eq(45)
+          expect(@market.sell(@item1, 40)).to eq(true)
+          expect(@vendor1.check_stock(@item1)).to eq(0)
+          expect(@vendor3.check_stock(@item1)).to eq(60)
+
+        end
+  end
 end
